aa8fe8f288dbaed3d7d27ca6589cacc14da0b7e3
EscapedSheep <sfgmdf001@126.com>
2020-08-12 23:18:52 +0800

    [maida] implements home page

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './home.less';
+import { Link } from 'react-router-dom';
+import calculatorImg from '../images/calculator.png';
+import timerImg from '../images/timer.png';
 
 const Home = () => {
-  return (<div className="home">
-    This is home page
-  </div>);
+  return (
+    <div className="home">
+      <div className="homeUpper">
+        <h1>在线实用工具</h1>
+      </div>
+      <div className="homeDown">
+        <div className="homeImgLink">
+          <Link>
+            <img src={calculatorImg} alt="calculator"></img>
+            <br></br>
+            计算器
+          </Link>
+        </div>
+        <div className="homeImgLink">
+          <Link>
+            <img src={timerImg} alt="timer"></img>
+            <br></br>
+            倒计时器
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
